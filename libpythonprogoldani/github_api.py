676e657eb355d593793d1d1bd8ac1fe74a809cd1
ggoldani <tech.goldani@gmail.com>
2021-08-20 21:42:16 -0300

    test

--- libpythonprogoldani/github_api.py
+++ libpythonprogoldani/github_api.py
@@ -10,4 +10,4 @@ def buscar_avatar(usuario):
     """
     url = f"https://api.github.com/users/{usuario}"
     resposta = requests.get(url)
-    return resposta.json()["avatar_url"]
+    return resposta.json()['avatar_url']
