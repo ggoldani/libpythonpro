78286dfc6f1062ecfecf6e317f3663d569fa1bc4
ggoldani <tech.goldani@gmail.com>
2021-08-27 20:01:56 -0300

    adicionando fixture

--- libpythonprogoldani/tests/test_spam/test_usuarios.py
+++ libpythonprogoldani/tests/test_spam/test_usuarios.py
@@ -1,25 +1,35 @@
+import pytest as pytest
+
 from libpythonprogoldani.spam.db import Conexao
 from libpythonprogoldani.spam.modelos import Usuario
 
+@pytest.fixture
+def conexao():
+    # Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear down
+    conexao_obj.fechar()
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture
+def sessao(conexao):
+    # Setup
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    # Tear Down
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Gustavo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Gustavo'), Usuario(nome='Goldani')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+
