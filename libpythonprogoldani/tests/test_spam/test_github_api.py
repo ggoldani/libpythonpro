ae47be50bcee2a2ce471f4d2b2d236ae4240297a
ggoldani <tech.goldani@gmail.com>
2021-09-13 17:45:04 -0300

    pytest-mock mocker

--- libpythonprogoldani/tests/test_spam/test_github_api.py
+++ libpythonprogoldani/tests/test_spam/test_github_api.py
@@ -9,8 +9,7 @@ from libpythonprogoldani import github_api
 def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/70818371?v=4'
-    resp_mock.json.return_value = {'login': 'ggoldani', 'id': 70818371,
-                                   'avatar_url': url}
+    resp_mock.json.return_value = {'login': 'ggoldani', 'id': 70818371, 'avatar_url': url}
     get_mock = mocker.patch('libpythonprogoldani.github_api.requests.get')
     get_mock.return_value = resp_mock
     return url
