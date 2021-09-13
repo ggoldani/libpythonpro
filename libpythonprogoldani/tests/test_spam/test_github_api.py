8c194bd5243304111ea93167372f35c85b623c56
ggoldani <tech.goldani@gmail.com>
2021-09-13 17:18:57 -0300

    pytest-mock mocker

--- libpythonprogoldani/tests/test_spam/test_github_api.py
+++ libpythonprogoldani/tests/test_spam/test_github_api.py
@@ -6,15 +6,14 @@ from libpythonprogoldani import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/70818371?v=4'
     resp_mock.json.return_value = {'login': 'ggoldani', 'id': 70818371,
                                    'avatar_url': url}
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonprogoldani.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
