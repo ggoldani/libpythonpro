10b26143a78d403991b0bfedeb3b33650bb02aba
ggoldani <tech.goldani@gmail.com>
2021-09-13 16:54:55 -0300

    isolamento de imports

--- libpythonprogoldani/tests/test_spam/test_github_api.py
+++ libpythonprogoldani/tests/test_spam/test_github_api.py
@@ -1,12 +1,27 @@
 from unittest.mock import Mock
 
+import pytest as pytest
+
 from libpythonprogoldani import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/70818371?v=4'
     resp_mock.json.return_value = {'login': 'ggoldani', 'id': 70818371,
-                                   'avatar_url': 'https://avatars.githubusercontent.com/u/70818371?v=4'}
+                                   'avatar_url': url}
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('ggoldani')
-    assert 'https://avatars.githubusercontent.com/u/70818371?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars.githubusercontent.com/u/3457115?v=4' == url
