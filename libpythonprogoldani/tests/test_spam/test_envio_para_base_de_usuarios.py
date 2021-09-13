baa7a9ac745e897e92397d3d0fb3a881d19f82b0
ggoldani <tech.goldani@gmail.com>
2021-09-13 15:42:50 -0300

    aulas do modulo pytools injeção de dependencia aula Mock

--- libpythonprogoldani/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonprogoldani/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,19 +1,10 @@
+from unittest.mock import Mock
+
 import pytest as pytest
 
 from libpythonprogoldani.spam.enviador_de_email import Enviador
 from libpythonprogoldani.spam.main import EnviadorDeSpam
 from libpythonprogoldani.spam.modelos import Usuario
-
-
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        self.parametros_de_envio = None
-        self.qtd_emails_enviados = 0
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_emails_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -31,24 +22,24 @@ class EnviadorMock(Enviador):
 def test_qnt_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'gustavo@gmail.com',
         'Assunto',
         'Conteudo'
     )
-    assert len(usuarios) == enviador.qtd_emails_enviados
+    assert len(usuarios) == enviador.enviar.call_count # metodo do mock que conta qnts vezes o metodo foi chamado
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Maciel', email='maciel@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'goldani@gmail.com',
         'Assunto',
         'Conteudo'
     )
-    assert enviador.parametros_de_envio == ('goldani@gmail.com', 'maciel@gmail.com', 'Assunto', 'Conteudo')
+    enviador.enviar.assert_called_once_with('goldani@gmail.com', 'maciel@gmail.com', 'Assunto', 'Conteudo')
